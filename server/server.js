2692052d33b2aab6b2a5d36367acdc0f2a24ed61
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2017-01-10 15:13:21 -0700

    cleaned up server code

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// GET /todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos})
@@ -17,35 +18,28 @@ app.get('/todos', (req, res) => {
     });
 });
 
-// GET /todos/_id/1234321
+// GET /todos/_id
 app.get('/todos/:id', (req, res) => {
     // res.send(req.params);
     var id = req.params.id;
 
-    // Valid id using isValid
-        // 404 - send back empty {}
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
-    
-    // findById
-        // success
-            // if todo - send back 200 with todo
-            // if not todo - send back 404 with empty body      
+      
     Todo.findById(id)  
         .then((todo) => {
             if (!todo) {
                 return res.status(404).send('Todo not found');
             }
 
-            res.status(200).send({todo});
-        // error
-            // 400 - and send empty body back            
+            res.status(200).send({todo});          
         }).catch((e) => {
             res.status(400).send();
         });
 });
 
+// POST /todos
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -63,46 +57,3 @@ app.listen(3000, () => {
 });
 
 module.exports = {app};
-// var newTodo = new Todo({
-//     text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save todo');
-// });
-
-// var myTodo = new Todo({
-//     text: 'Grill steak'
-// });
-
-// myTodo.save().then((doc) => {
-//     console.log('Saved todo', doc);
-// }, (e) => {
-//     console.log('Unable to save todo');
-// });
-
-// User model
-// email - required, trimmed, type: String, minlength: 1
-
-// Create a user with/without email
-// var su = new User({
-//     email: null
-// });
-
-// var user = new User({
-//     email: 'cochsen@lycos.com'
-// });
-
-// su.save().then((rec) => {
-//     console.log('Saved user', rec);
-// }, (e) => {
-//     console.log('Unable to save user: ', e);
-// });
-
-// user.save().then((rec) => {
-//     console.log('Saved user', rec);
-// }, (e) => {
-//     console.log('Unable to save user: ', e);
-// });
