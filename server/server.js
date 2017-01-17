dfb1cfe5bb74ed70f1779205bdbfb46b432b438d
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2017-01-16 19:24:41 -0700

    added POST /users/login

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
    res.send(req.user);
 });
@@ -27,6 +28,19 @@ app.post('/users', (req, res) => {
         res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
+    });
+});
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+    var credentials = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(credentials.email, credentials.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
