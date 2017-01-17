4b5110e72d39585d70895a4d929873a88335fa58
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2017-01-17 16:40:55 -0700

    added DELETE /users/token/me

--- server/server.js
+++ server/server.js
@@ -44,6 +44,15 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+// DELETE /users/me/token
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
 // GET /todos
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
